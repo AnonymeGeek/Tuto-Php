b4d1ad5df9b5a674e49654cb6c928a58705d3152
AnonymeGeek <49288850+AnonymeGeek@users.noreply.github.com>
2019-04-06 22:06:35 +0200

    Essai footer message de connection

--- database.php
+++ database.php
@@ -8,6 +8,7 @@
 	try{
 		$db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS );
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+                 ?><footer ><?="Connection ok"?></footer><?php
 		
 	} catch(PDOException $erreur){
 		echo $erreur;
