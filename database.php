419e03b1df50db1d11a07ae4919105bb2da19793
Navid <navid.adler@gmail.com>
2019-04-06 22:14:43 +0200

    Revert "Del car Inutile"
    This reverts commit a84cc3471610d4dd63feae80898edacbbae707e8.

--- database.php
+++ database.php
@@ -7,9 +7,9 @@
 
 	try{
 		$db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS );
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              
-
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+                 ?><footer ><?="Connection ok"?></footer><?php
+		
 	} catch(PDOException $erreur){
 		echo $erreur;
 	}
